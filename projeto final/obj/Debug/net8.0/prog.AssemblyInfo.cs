45649cbccfd364a4df0f87f4400914c69a3dc6bb
luizike2002 <luiz.grazziotin@gmail.com>
2024-06-25 11:58:10 -0300

    projeto final

--- projeto final/obj/Debug/net8.0/prog.AssemblyInfo.cs
+++ projeto final/obj/Debug/net8.0/prog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2e9b8186261e3280968200a5a6a022d82a335e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("prog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
